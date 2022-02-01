d48d975ee8061c9110e3a49cae7cd576fa43f7e6
Lee Pavelich <leepavelich@gmail.com>
2022-02-01 04:19:02 +0000

    Implement character counter function, which dynamically changes the count under the new tweet field and shows red for negative values

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  console.log('loaded')
+  $('#tweet-text').keyup(function() {
+    const max = 140;
+    let count = $(this).val().length;
+
+    let char = max - count;
+    const counter = $(this).closest('form').find('.counter');
+
+    if (char < 0) {
+      counter.val(-char);
+      counter.css('color', 'red');
+    } else {
+      counter.val(char);
+      counter.css('color', '#545149')
+    }
+  })
 });
