98a08ea300406cf0fbbf6f476a87ece9e5345e18
Lee Pavelich <leepavelich@gmail.com>
2022-02-02 17:17:53 +0000

    Initial implementation of createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,27 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(() => {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
+  
+  // const createTweetElement = (tweet) => {
+  //   return $(`<article class="tweet">Hello world</article>`);
+  // }
+  
+  // const $tweet = createTweetElement(tweetData);
 
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  
+  console.log($tweet);
+  $('.container').append($tweet);
+})
